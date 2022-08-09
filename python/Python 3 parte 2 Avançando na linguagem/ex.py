3040c7da5dc48cf27936b5191ca54be6334a4ad4
Lukxus <83554105+Lukxus@users.noreply.github.com>
2022-08-09 15:19:10 -0300

    Add files via upload

---
+++ python/Python 3 parte 2 Avançando na linguagem/ex.py
@@ -0,0 +1,19 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not acertou and not enforcou):
+        print("Jogando...")
+
+    print("Fim do jogo")
+
+print(__name__)
+
+if(__name__ == "__main__"):
+    pass#jogar()
